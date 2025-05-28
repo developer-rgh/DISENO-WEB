99112d1afc33a5e8e40ff0b0585c4bc9932b3608
developer-rgh <11ruben.garcia10@gmail.com>
2025-05-28 17:46:33 -0600

    Variables en JavaScript

---
+++ js/VariablesJAvaScript.js
@@ -0,0 +1,16 @@
+// Variables 
+var nombre = "Marc Marquez";
+let edad = 31;
+const FECHANACIMIENTO= "10/09/1992";
+
+alert("Mi nombre es " + nombre +  " y mi fecha de nacimiento es el " + FECHANACIMIENTO);
+console.log("Edad:", edad);
+
+
+// Ejemplo de varibales 2
+let lugarNacimiento = prompt("Ingresar tu lugar de nacimiento: ");
+console.log("Lugar de nacimiento: ", lugarNacimiento);
+alert("Lugar de Nacimiento: " + lugarNacimiento);
+
+
+
