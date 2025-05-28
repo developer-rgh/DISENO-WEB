9420258b0abfa91699db07624e0200c6d027c249
developer-unsij <rubengarcia@unsij.edu.mx>
2025-05-28 15:38:37 -0600

    Primer commit del curso de diseño web

---
+++ js/ConsolaJavaScript.js
@@ -0,0 +1,7 @@
+function mostrarMensajes(){
+    console.log("Este es un mensaje desde la consola de JS.");
+    console.log("Suma: ", 10 + 15);
+    console.info("Mostrando mensaje de información desde la consola de JS.");
+    console.warn("Mostrando mensaje de advertencia desde la consola de JS.");
+    console.error("Mostrando mensaje de error desde la consola de JS.");
+}
